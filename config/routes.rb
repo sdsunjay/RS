13906161bb0feac13d84a001f569e282b30d2ea1
Sunjay Dhama <sdsunjay73@yahoo.com>
2014-01-26 00:06:41 +0000

    Added some of Wills html and css for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get "home/index"
 
   devise_for :users
-  resources :users
+  get 'home' => 'home#index', as: :after_sign_out_path_for
+  root 'home#index'
+  #resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
